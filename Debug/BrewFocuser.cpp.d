8493f1740211911f5ad0eb722297dbdd7ab63232
EridaniBrew <EridaniBrew@users.noreply.github.com>
2017-05-05 10:42:10 -0700

    Add files via upload

---
+++ Debug/BrewFocuser.cpp.d
@@ -0,0 +1,28 @@
+C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\BrewFocuser.cpp.o: \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\BrewFocuser.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\Globals.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\AccelStepper/AccelStepper.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_Motor_Shield_V2/Adafruit_MotorShield.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_Motor_Shield_V2/utility/Adafruit_MS_PWMServoDriver.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_Motor_Shield_V2/utility/Adafruit_MS_PWMServoDriver.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_MCP9808/Adafruit_MCP9808.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\BaseProtocol.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\EasyFocus.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\Robofocus.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\Optec.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\EepromUtil.h
---
+++ Debug/Robofocus.cpp.d
@@ -0,0 +1,27 @@
+C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\Robofocus.cpp.o: \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\Robofocus.cpp \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\Robofocus.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\BaseProtocol.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\Globals.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\AccelStepper/AccelStepper.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_Motor_Shield_V2/Adafruit_MotorShield.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_Motor_Shield_V2/utility/Adafruit_MS_PWMServoDriver.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_Motor_Shield_V2/utility/Adafruit_MS_PWMServoDriver.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h \
+ C:\Users\robert\Dropbox\BrewSky\Programs\libraries\Adafruit_MCP9808/Adafruit_MCP9808.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\MotorIO.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\TempComp.h \
+ C:\Users\robert\AppData\Local\Temp\VMBuilds\BrewFocuser\uno\Debug\EepromUtil.h
